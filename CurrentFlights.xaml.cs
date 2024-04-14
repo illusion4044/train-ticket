39af7ad1d5228fb64d4bb6245411cabfd8d0de8e
Andrii <117983189+illusion4044@users.noreply.github.com>
2024-04-14 21:40:13 +0300

    Add files via upload

---
+++ CurrentFlights.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace Avia1
+{
+    public partial class CurrentFlightsWindow : Window
+    {
+        public static List<Flight> CurrentFlightsList = new List<Flight>();
+
+        public CurrentFlightsWindow()
+        {
+            InitializeComponent();
+            AddCurrentFlight("Київ", "One Way", "Бізнес клас");
+            AddCurrentFlight("Біла Церква", "One Way", "Економ клас");
+            AddCurrentFlight("Харків", "Return", "Бізнес клас");
+            AddCurrentFlight("Одеса", "Return", "Бізнес клас");
+        }
+
+        public void AddCurrentFlight(string destination, string tripType, string ticketClass)
+        {
+            CurrentFlightsList.Add(new Flight
+            {
+                Destination = destination,
+                TripType = tripType,
+                Class = ticketClass
+            });
+
+            CurrentFlightsListBox.Items.Add($"{destination}, {tripType}, {ticketClass}");
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
